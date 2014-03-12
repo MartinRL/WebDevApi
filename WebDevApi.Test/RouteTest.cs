3bc01b4a4d49662386ba6438fae76026c3fe617e
Martin Rosén-Lidholm <mrol@telenor.dk>
2014-03-12 14:03:19 +0100

    add route test

---
+++ WebDevApi.Test/RouteTest.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Nancy;
+using Nancy.Testing;
+using Xunit;
+
+namespace WebDevApi.Test
+{
+    public class RouteTest
+    {
+		[Fact]
+		public void Should_return_status_ok_when_route_exists()
+		{
+			var bootstrapper = new DefaultNancyBootstrapper();
+			var browser = new Browser(bootstrapper);
+
+			var response = browser.Get("/customer/1", with => with.HttpRequest());
+
+			response.StatusCode.Should().Be(HttpStatusCode.OK);
+		}
+    }
+}
