b0598c6f7457f8ce4889b1a466d9fe55b829a601
Martin Rosén-Lidholm <mrol@telenor.dk>
2014-03-13 12:33:14 +0100

    add test

--- WebDevApi.Test/CustomerResourceTest.cs
+++ WebDevApi.Test/CustomerResourceTest.cs
@@ -13,10 +13,10 @@ namespace WebDevApi.Test
 			var browser = new Browser(new Bootstrapper());
 
 			var response = browser.Get("/customers/1", with =>
-            {
-                with.Header("accept", "application/json");
-                with.HttpRequest();
-            });
+			{
+				with.Header("accept", "application/json");
+				with.HttpRequest();
+			});
 
 			response.StatusCode.Should().Be(HttpStatusCode.OK);
 			response.Body.AsString().Should().Be(
@@ -44,6 +44,79 @@ namespace WebDevApi.Test
 		]
 		""receivePromomotionsPermissions"": {
 			""sms"": false,
+			""email"": true,
+			""phoneCall"": false
+		}
+	}
+}");
+		}
+
+		[Fact]
+		public void put_to_existing_id_should_update_customer()
+		{
+			var browser = new Browser(new Bootstrapper());
+
+			var response = browser.Put("/customers/1", browserContext => browserContext.Body(
+@"{
+	""customer"": {
+		""id"": 1,
+		""cpr"": 180173XXXX,
+		""secondaryPhoneNumber"": ""+46707318625"",
+		""name"": {
+			""first"": ""Martin"",
+			""last"": ""Rosén-Lidholm""
+		}
+		""email"": ""mrl@mvno.dk"",
+		""address"": {
+			""street"": ""Nils Anderssons gata"",
+			""houseNumber"": 12,
+			""postalCode"": 21836,
+			""city"": ""Bunkeflostrand"",
+			""country"": ""Sverige"",
+		}
+		""balance"": 322.50,
+		""electiveServices"": [
+			{ ""name"": ""5 GB data"" }, 
+			{ ""name"": ""Udlandsopkald"" },
+		]
+		""receivePromomotionsPermissions"": {
+			""sms"": true,
+			""email"": true,
+			""phoneCall"": false
+		}
+	}
+}")).Then.Get("/customers/1", with =>
+				{
+					with.Header("accept", "application/json");
+					with.HttpRequest();
+				});
+
+			response.StatusCode.Should().Be(HttpStatusCode.OK);
+			response.Body.AsString().Should().Be(
+@"{
+	""customer"": {
+		""id"": 1,
+		""cpr"": 180173XXXX,
+		""secondaryPhoneNumber"": ""+46707318625"",
+		""name"": {
+			""first"": ""Martin"",
+			""last"": ""Rosén-Lidholm""
+		}
+		""email"": ""mrl@mvno.dk"",
+		""address"": {
+			""street"": ""Nils Anderssons gata"",
+			""houseNumber"": 12,
+			""postalCode"": 21836,
+			""city"": ""Bunkeflostrand"",
+			""country"": ""Sverige"",
+		}
+		""balance"": 322.50,
+		""electiveServices"": [
+			{ ""name"": ""5 GB data"" }, 
+			{ ""name"": ""Udlandsopkald"" },
+		]
+		""receivePromomotionsPermissions"": {
+			""sms"": true,
 			""email"": true,
 			""phoneCall"": false
 		}
