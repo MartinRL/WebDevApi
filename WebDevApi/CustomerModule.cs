b1594970f00655cf0c348fa51dbdebb9732999fe
Martin Rosén-Lidholm <mrol@telenor.dk>
2014-03-13 10:58:18 +0100

    add implementation

---
+++ WebDevApi/CustomerModule.cs
@@ -0,0 +1,41 @@
+﻿using Nancy;
+
+namespace WebDevApi
+{
+	public class CustomerModule : NancyModule
+	{
+		public CustomerModule()
+		{
+			Get ["/customers/{id}"] = _ =>
+@"{
+	""customer"": {
+		""id"": 1,
+		""cpr"": 180173XXXX,
+		""secondaryPhoneNumber"": ""+46707318625"",
+		""name"": {
+			""first"": ""Martin"",
+			""last"": ""Rosén-Lidholm""
+		}
+		""email"": ""mrol@telenor.dk"",
+		""address"": {
+			""street"": ""Nils Anderssons gata"",
+			""houseNumber"": 12,
+			""postalCode"": 21836,
+			""city"": ""Bunkeflostrand"",
+			""country"": ""Sverige"",
+		}
+		""balance"": 322.50,
+		""electiveServices"": [
+			{ ""name"": ""5 GB data"" }, 
+			{ ""name"": ""Udlandsopkald"" },
+		]
+		""receivePromomotionsPermissions"": {
+			""sms"": false,
+			""email"": true,
+			""phoneCall"": false
+		}
+	}
+}";
+		}
+	}
+}
